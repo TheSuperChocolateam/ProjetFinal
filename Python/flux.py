383d45490753659f7499bb91ed6add90af44e71b
Jessica <teamlille@mail.fr>
2022-09-26 16:56:18 +0200

    mise en place de la récupération du flux rss

--- Python/flux.py
+++ Python/flux.py
@@ -1,6 +1,31 @@
 import feedparser
 
-NewsFeed = feedparser.parse("https://www.cert.ssi.gouv.fr/")
+print ("FLUX RSS CERT")
+NewsFeed = feedparser.parse("https://www.cert.ssi.gouv.fr/alerte/feed/")
 print ('Number of RSS posts :', len(NewsFeed.entries))
-entry = NewsFeed.entries[1]
-print ('Post Title :', entry.title)
+
+i=0
+while i != 6:
+    entry = NewsFeed.entries[i] 
+    print (entry.published) 
+    print ("******")
+    print ("Titre :", entry.title)
+    print ("Description :", entry.summary) 
+    print ("------News Link--------")
+    print (entry.link) 
+    i+=1
+
+print ("FLUX RSS NVD")
+NewsFeed = feedparser.parse("https://www.nist.gov/news-events/news/rss.xml")
+print ('Number of RSS posts :', len(NewsFeed.entries))
+
+j=0
+while j != 6:
+    entry = NewsFeed.entries[i] 
+    print (entry.published) 
+    print ("******")
+    print ("Titre :", entry.title)
+    print ("Description :", entry.summary) 
+    print ("------News Link--------")
+    print (entry.link) 
+    j+=1
