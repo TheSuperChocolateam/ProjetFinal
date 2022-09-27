d6cd040d60ca4d92b00180e02d6b1af0ed5c5600
Jessica <teamlille@mail.fr>
2022-09-27 16:48:36 +0200

    affichage des données RSS dans un tableau en html

--- Python/main.py
+++ Python/main.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template, request, send_file, redirect, url_for, Response, redirect
-from flux import flux
+import feedparser
 
 app = Flask(__name__)
 
-import sys
-import os
+NewsFeedNist = feedparser.parse("https://www.nist.gov/news-events/news/rss.xml")
+tab_entries= []
 
-@app.route('/', methods=['GET', 'POST'])
-def menu():
-    entry=flux() 
-    return render_template("index.html", entry=entry)
+print(type(tab_entries))
+
+j=0
+while j != 3:
+    entry = NewsFeedNist.entries[j] 
+    date = entry.published
+    title = entry.title  
+    description = entry.summary
+    link = entry.link
+    tab_entries.append({"date":date, "title":title, "description":description, "link":link})
+    j+=1
+
+print(tab_entries[0])
+
+@app.route('/')
+def menu():    
+    return render_template("index.html", len= len(tab_entries), entries=tab_entries)
     
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8001)
+    app.run(debug=True, host='0.0.0.0', port=5501)
